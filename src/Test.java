63fc1825eba6d198e24dbb693a3e4abf8805c145
banduo <139312175@qq.com>
2018-06-14 17:00:48 +0800

    wanwan 01

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+/**
+ * Function: (这里用一句话描述这个类的作用)
+ *
+ * @author lixin
+ * @date 2018/6/14 17:00
+ * @see
+ */
+public class Test {
+    public static void main(String[] args) {
+        String a = "klll";
+        System.out.println(a);
+    }
+}
